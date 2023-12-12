2cf5553f63ba52160c1e7d020dd6af6bb8f7b076
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2023-12-13 00:25:32 +0100

    fixed error

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,7 +9,8 @@ import random
 import asyncio
 from typing import Generator
 
-async def async_generator() -> Generator[float, None, None] :
+
+async def async_generator() -> Generator[float, None, None]:
     """
     Async generator
     """
