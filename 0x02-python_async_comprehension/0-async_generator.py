22fe17a4e6df7aba9c5672600fe7443c88776232
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2023-12-13 00:27:39 +0100

    fixed error

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-This Module contain  a coroutine called async_generator that takes no arguments.
-The coroutine will loop 10 times, each time asynchronously
-wait 1 second, then yield a random number between 0 and 10.
+This Module contain  a coroutine called async_generator that
+takes no arguments.The coroutine will loop 10 times, each time
+asynchronously wait 1 second, then yield a random number between 0 and 10.
 """
 
 import random
