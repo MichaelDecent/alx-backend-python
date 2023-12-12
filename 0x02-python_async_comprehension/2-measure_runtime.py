d2d49de516da07dfac57f27819b2ee2dff14651e
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2023-12-13 00:14:29 +0100

    added solution to task 0 - 4

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This Module cotains a function measure_runtime should
+measure the total runtime and return it
+"""
+import time
+import asyncio
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime():
+    """
+    measures the total runtime and return it.
+    """
+    start = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    elapsed = time.perf_counter() - start
+
+    return elapsed
