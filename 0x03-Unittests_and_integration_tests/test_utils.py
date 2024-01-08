622e5e0a627d82c9d7fcd469a569c97ab9867f8a
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-08 15:36:03 +0100

    added solution to task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,27 +1,30 @@
-#!/usr/bin/python3
-"""
-This Module contain a unit test for access_nested_map
-"""
-from utils import access_nested_map
-from unittest import TestCase
-from parameterized import parameterized, param
-from typing import Mapping, Sequence, Union, Dict
-
-
-class TestAccessNestedMap(TestCase):
-    """
-    This class contain Unit test for a function access_nested_map
-    """
-
-    @parameterized.expand(
-        [
-            ({"a": 1}, ("a",), 1),
-            ({"a": {"b": 2}}, ("a",), {"b": 2}),
-            ({"a": {"b": 2}}, ("a", "b"), 2),
-        ]
+import unittest
+from unittest.mock import Mock, patch
+from utils import access_nested_map, get_json, memoize
+from parameterized import parameterized
+from typing import (
+        Mapping,
+        Sequence,
+        Any,
+        Dict,
+        Callable,
+        Union
     )
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """The test fixture for testing the access_nested_map method
+    in the utils modules"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
     def test_access_nested_map(
-        self, nested_map: Mapping, path: Sequence, expected_result: Union[Dict, int]
-    ) -> None:
-        """test function for the function access_nested_map()"""
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+            self,
+            nested_map: Mapping,
+            path: Sequence,
+            result: Union[Dict, int]) -> None:
+        """Test the method access_nested_map"""
+        self.assertEqual(access_nested_map(nested_map, path), result)
