4cbdbcf0a734478bbb9c0d79a92e9c4e346d6e34
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-08 14:47:17 +0100

    added solution to task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -11,14 +11,18 @@ from typing import Mapping, Sequence, Any
 class TestAccessNestedMap(TestCase):
     """
     This class contain Unit test for a function test_access_nested_map
-
     """
-    @parameterized.expand([
-    param(nested_map={"a": 1}, path=("a",), expected_result=1),
-    param(nested_map={"a": {"b": 2}}, path=("a",), expected_result={'b': 2}),
-    param(nested_map={"a": {"b": 2}}, path=("a", "b"), expected_result=2),
-    ])
 
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected_result: Any) -> Any:
+    @parameterized.expand(
+        [
+            param(nested_map={"a": 1}, path=("a",), expected_result=1),
+            param(nested_map={"a": {"b": 2}}, path=(
+                "a",), expected_result={"b": 2}),
+            param(nested_map={"a": {"b": 2}}, path=(
+                "a", "b"), expected_result=2),
+        ]
+    )
+    def test_access_nested_map(
+        self, nested_map: Mapping, path: Sequence, expected_result: Any
+    ) -> Any:
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
-    
