3a4484f2f7100ebeb98ef9aab05c79710c534e02
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-08 22:40:03 +0100

    added solution to task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,30 +1,48 @@
-import unittest
-from unittest.mock import Mock, patch
-from utils import access_nested_map, get_json, memoize
+#!/usr/bin/python3
+"""
+This Module contain a unit test for access_nested_map
+"""
+from utils import access_nested_map
+from unittest import TestCase
 from parameterized import parameterized
-from typing import (
-        Mapping,
-        Sequence,
-        Any,
-        Dict,
-        Callable,
-        Union
+from typing import Mapping, Sequence, Union, Dict
+
+
+class TestAccessNestedMap(TestCase):
+    """
+    This class contain Unit test for a function access_nested_map
+    """
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
     )
-
-
-class TestAccessNestedMap(unittest.TestCase):
-    """The test fixture for testing the access_nested_map method
-    in the utils modules"""
-
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
     def test_access_nested_map(
             self,
             nested_map: Mapping,
             path: Sequence,
-            result: Union[Dict, int]) -> None:
-        """Test the method access_nested_map"""
-        self.assertEqual(access_nested_map(nested_map, path), result)
+            expected_result: Union[Dict, int]) -> None:
+        """test function for the function access_nested_map()"""
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), KeyError('a')),
+            ({"a": 1}, ("a", "b"), KeyError('b')),
+        ]
+    )
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Mapping,
+            path: Sequence,
+            expected_exception: Union[Dict, int]) -> None:
+        """
+        Test function for exceptions in access_nested_map()
+        """
+
+        with self.assertRaises(type(expected_exception)) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), str(expected_exception))
--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -39,9 +39,9 @@ def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
 
     return nested_map
 
+
 def get_json(url: str) -> Dict:
-    """Get JSON from remote URL.
-    """
+    """Get JSON from remote URL."""
     response = requests.get(url)
     return response.json()
 
@@ -66,7 +66,7 @@ def memoize(fn: Callable) -> Callable:
 
     @wraps(fn)
     def memoized(self):
-        """"memoized wraps"""
+        """ memoized wraps"""
         if not hasattr(self, attr_name):
             setattr(self, attr_name, fn(self))
         return getattr(self, attr_name)
